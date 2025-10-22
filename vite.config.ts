a14820fb6796e5cc9f1e48d5198a0c904346fd2d
Spearhead Morrow <spearheadmorrow@gmail.com>
2025-10-22 10:55:52 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-const base = process.env.VITE_BASE || process.env.BASE || '/';
+const base = process.env.VITE_BASE || process.env.BASE || '/online-meeting-list';
 
 export default defineConfig({
   base,
